8ca0428862e67a1c239505b0f9461ff21d7813e7
Kamil Korczyński <k.korczynski@selleo.com>
2019-08-19 22:18:09 +0200

    finish business communication section styling

--- src/components/BusinessCommunication.js
+++ src/components/BusinessCommunication.js
@@ -2,26 +2,38 @@ import React from "react";
 import Button from "./Button";
 
 import logo from "../assets/images/business_communication.png";
+import play from "../assets/images/play.svg";
 
 export default function BusinessCommunication(props) {
   return (
-    <div className="buscom">
-      <img src={logo} alt="" className="buscom__image" />
-      <div className="buscom__overview">
-        <h2>Communicate. Collaborate. Complete.</h2>
-        <p className="buscom__description">
-          The remote collaboration tool to help you communicate effectively and
-          work productively.
-        </p>
-        <label htmlFor="email">Email address</label>
-        <input type="email" className="buscom__input" id="email"/>
-        <div className="buscom__try-it">
-          <div>
-            Try Droplr <strong>PRO free for 30 days.</strong>
-            <br />
-            No credit card needed. <a className="underline">Privacy Policy</a>
+    <div className="buscom__wrapper">
+      <div className="buscom__content">
+        <div className="buscom__image">
+          <img src={logo} alt="" className="buscom__image" />
+          <div className="play-button">
+            <img src={play} alt="" />
           </div>
-          <Button text="Get Started" appearance="-success navbar__element" />
+        </div>
+        <div className="buscom__overview">
+          <span className="buscom__header">
+            Communicate. Collaborate. Complete.
+          </span>
+          <p className="buscom__description">
+            The remote collaboration tool to help you communicate effectively
+            and work productively.
+          </p>
+          <div className="form__item">
+            <label htmlFor="email">Email address</label>
+            <input type="email" className="buscom__input" id="email" />
+          </div>
+          <div className="buscom__try-it">
+            <div>
+              Try Droplr <strong>PRO free for 30 days.</strong>
+              <br />
+              No credit card needed. <a className="underline">Privacy Policy</a>
+            </div>
+            <Button text="Get Started" appearance="-success navbar__element" />
+          </div>
         </div>
       </div>
     </div>
