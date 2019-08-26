24ac8e4cb5ba9628d6f41eda3176c7e8cff4b783
Kamil Korczyński <k.korczynski@selleo.com>
2019-08-26 20:40:02 +0200

    rwd navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 import logo from "../assets/images/logo.svg";
+import menu from "../assets/images/menu.svg";
 
 export default function Navbar(props) {
+  const [isMenuOpen, toggleMenu] = useState(false);
+
   return (
     <div className="navbar">
-      <img src={logo} alt="logo" className="navbar__logo" />
-      <div className="navbar__elements">
-        <a className="navbar__element">Features</a>
-        <a className="navbar__element">Apps</a>
-        <a className="navbar__element">Pricing</a>
-        <a className="navbar__element">Knowledge Base</a>
-        <a className="navbar__element">Business</a>
-        <Button text="LOG IN" appearance="-secondary navbar__element" />
-        <Button text="SIGN UP" appearance="-primary navbar__element" />
+      <div>
+        <div className="navbar__logo-and-menu">
+          <img src={logo} alt="logo" className="navbar__logo" />
+
+          <img
+            src={menu}
+            alt="menu"
+            className="navbar__menu"
+            onClick={() => toggleMenu(isMenuOpen => !isMenuOpen)}
+          />
+        </div>
       </div>
+        <div className={`navbar__elements ${isMenuOpen ? '' : '-hide'}`}>
+          <a className="navbar__element">Features</a>
+          <a className="navbar__element">Apps</a>
+          <a className="navbar__element">Pricing</a>
+          <a className="navbar__element">Knowledge Base</a>
+          <a className="navbar__element">Business</a>
+          <Button text="LOG IN" appearance="-secondary navbar__element" />
+          <Button text="SIGN UP" appearance="-primary navbar__element" />
+        </div>
     </div>
   );
 }
